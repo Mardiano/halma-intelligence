68a13a6acd9c96333413a7e224f0d0312f10eca5
Mardiano <13518039@std.stei.itb.ac.id>
2020-10-20 23:32:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+import math
+import copy
+
 pygame.init()
+
+
 # Memulai pygame
 
 win = pygame.display.set_mode((1280,720)) # window dari pygamenya, ukurannya 1280x720 pixel
@@ -158,7 +163,6 @@ class startstate(object):
 
 ###############################################################################################################################################################################################################################################################################################
 # ^^^ dia namanya shafa ^^^ #
-
 def redrawGameWindow():
     #buat ngedraw game window
     if start:
@@ -171,13 +175,15 @@ def cekTetangga(x,y,a,b):
     # buat cek tetangga?
     return (x-a == 1 and y-b == 1) or (a-x == 1 and y-b == 1) or (x-a == 1 and b-y == 1) or (a-x == 1 and b-y == 1) or (x-a == 0 and y-b == 1) or (x-a == 1 and y-b == 0) or (x-a == 0 and b-y == 1) or (a-x == 1 and y-b == 0)
 
+'''
 objectiveValue8 = [[0,15,30,30,35,35,35,35], [15, 35, 40, 45, 45, 45, 45, 45], [30, 40, 55, 55, 55, 55, 55, 55], [30, 45, 55, 65, 65, 65, 65, 65], [35, 45, 55, 65, 75, 75, 75, 85], [35, 45, 55, 65, 75, 75, 75, 85], [35, 45, 55, 65, 75, 90, 95, 95], [35, 45, 55, 65, 85, 90, 95, 100]]
 objectiveValue10 = [[0,15,20,25,30,35,35,35,35,35], [15,25,30,35,40,40,40,40,40,40], [20,30,35,45,45,45,45,45,45,45], [25,35,45,55,55,55,55,55,55,55], [30,40,45,55,65,65,65,65,65,65], [35,40,45,55,65,70,70,70,70,80], [35,40,45,55,65,70,80,80,85,85], [35,40,45,55,65,70,80,85,90,90], [35,40,45,55,65,70,85,90,95,95], [35,40,45,55,65,80,85,90,95,100]]
 objectiveValue16 = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
 
-def fungsiObjektif(x,y,size):
+def cellFungsiObjektif(x,y,size,player):
     if (size == 8):
         global objectiveValue8
+        if player =
         return objectiveValue8[x][y]
     elif (size == 10):
         global objectiveValue10
@@ -185,28 +191,148 @@ def fungsiObjektif(x,y,size):
     else:
         global objectiveValue16
         return objectiveValue16[x][y]
-        
+
+def fungsiObjektif(papan, player):
+    res = 0
+    for isi in papan.isi:
+        res += cellFungsiObjektif()
+
+def possibleMoveJump(papan, pioneer):
+    allMove = []
+    for i in range ((pioneer.x)-2, (pioneer.x)+2)
+        for j in range ((pioneer.y)-2, (pioneer.y)+2)
+        #ini masih belum selesai
+        if (not(cekTetangga(pioneer.x,pioneer.y,i,j)) and not(pioneer.x == i and pioneer.y == j) and i >= 0 and i <= papan.x and j>=0 and j<= papan.y):
+            if (papan.isi[i][j].status == 0 and papan.isi[i][j].owner):
+                papanTemp = copy.deepcopy(papan)
+                papanTemp.isi[pioneer.x][pioneer.y].status = 0
+                papanTemp.isi[pioneer.x][pioneer.y].owner = 2
+                papanTemp.isi[pioneer.x][pioneer.y].camp = 2
+                papanTemp.isi[i][j].status = pioneer.status
+                papanTemp.isi[i][j].owner = pioneer.owner
+                allmove.append(papanTemp)
+
+    for move in allMove:
+        allMove.append(recursiveJump(papan, pioneer, pioneer.x, )
+
+def recursiveJump(papan, pioneer, lastX, lastY):
+    allMove = []
+    return allMove
+
+def oneJump
+    
 
 
-def minimax(startstate):
-    bestScore = -999
-    for i in range startstate/
+def possibleMove(papan, pioneer):
+    #Move 1 langkah
+    allMove = [] 
+    for i in range ((pioneer.x)-1, (pioneer.x)+1)
+        for j in range ((pioneer.y)-1, (pioneer.y)+1)
+            if (not(pioneer.x == i and pioneer.y == j) and i >= 0 and i <= papan.x and j>=0 and j<= papan.y):
+                if (papan.isi[i][j].status == 0):
+                    # if udah masuk camp atau udah keluar home jadi gaboleh balik ke posisi yang sama
+                    papanTemp = copy.deepcopy(papan)
+                    papanTemp.isi[pioneer.x][pioneer.y].status = 0
+                    papanTemp.isi[pioneer.x][pioneer.y].owner = 2
+                    papanTemp.isi[pioneer.x][pioneer.y].camp = 2
+                    papanTemp.isi[i][j].status = pioneer.status
+                    papanTemp.isi[i][j].owner = pioneer.owner
+                    allmove.append(papanTemp)
+
+    allMove.append(possibleMoveJump(papan, pioneer))
+    return allMove
+'''
+
+def possibleMove(papan, pion):
+    countLoop = 1
+    sedangLoncat = False
+    allMove = []
+    for i in range(papan.x):
+        for j in range(papan.x):
+            if (countLoop > 0) and not ((pion.camp == 0 and papan.isi[i][j].camp == pion.owner+1) or (pion.camp != 0 and papan.isi[i][j].camp == 0 and pion.owner+1 != pion.camp)):
+                if (sedangLoncat == False and cekTetangga(pion.x,pion.y,papan.isi[i][j].x,papan.isi[i][j].y)):
+                    if (papan.isi[i][j].status == 0):
+                        allMove.append(papan.isi[i][j])
+                    if (papan.isi[i][j].status == 1 or papan.isi[i][j].status == 3):
+                        tetanggaYangInginKuloncati = papan.isi[i][j]
+                        sedangLoncat = True
+                        while sedangLoncat:
+                            a = math.floor((i + pion.x)/2) #bidak musuh
+                            b = math.floor((j + pion.y)/2)
+                            c = (i + pion.x)/2
+                            d = (j + pion.y)/2
+                            for k in range(papan.x):
+                                for l in range(papan.x):
+                                    if not ((pion.camp == 0 and papan.isi[k][l].camp == pion.owner+1) or (pion.camp != 0 and papan.isi[k][l].camp == 0 and pion.owner+1 != pion.camp)):
+                                        if (cekTetangga(a,b,pion.x,pion.y) and cekTetangga(k,l,a,b) and (c.is_integer() and d.is_integer() > 0) and (((papan.isi[a][b].status == 1) or (papan.isi[a][b].status == 3)) and ((papan.isi[pion.x][pion.y].status == 4) or (papan.isi[pion.x][pion.y].status == 2)))):
+                                            allMove.append(papan.isi[k][l])
+                                            for m in range(papan.x):
+                                                for n in range(papan.x):
+                                                    if (papan.isi[m][n].status == 1 or papan.isi[m][n].status == 3) and (papan.isi[m][n].x != tetanggaYangInginKuloncati.x and papan.isi[m][n].y != tetanggaYangInginKuloncati.y):
+                                                        tetanggaYangInginKuloncati = papan.isi[m][n]
+                                                    else:
+                                                        sedangLoncat = False
+    return allMove
+
+def max(listPapan, player, depth):
+    if (depth == 0):
+        return fungsiObjektif(listPapan[0], player)
+    min(listPapan, player, depth-1)
+
+def min(papan, player, depth):
+    if (depth == 0):
+        return fungsiObjektif(listPapan[0])
+    
+    min(listPapan, player, depth-1)
+
+def minimax(board, player, depth, score):
+    result = checkWinner()
+    if (result != null):
+        return result
+
+    pion = []
+    for i in range (len(board.isi)):
+        for j in range (len(board.isi[i])):
+            #Pion bot
+            if (papan.isi[i][j].owner == player):
+                pion.append(board.isi[i][j])
+
+    score = -9999
+    allMove = []
+    allPapan = []
+    for bidak in pion:
+        for move in (possibleMove(papan, bidak)):
+            papanTemp = copy.deepcopy(board)
+            papanTemp.isi[bidak.x][bidak.y].status = 0
+            papanTemp.isi[bidak.x][bidak.y].owner = 2
+            papanTemp.isi[i][j] = copy.deepcopy(move)
+            allPapan.append(papanTemp)
+
+    bestValue = -(math.inf)
+    bestPapan = papan()
+    for papan in allPapan:
+        val = min(papan, player)
+        if (val > bestValue):
+            bestValue = val
+            bestX = pion.x
+            bestY = pion.y
 
 
-def min(papan, depth):
+# def bestMove(papan, player):
+#     #papan berkelas startstate
+#     #color: 0 merah, 1 kuning
+#     bestScore = -999
+#     score = minimax(papan, player, 0, false, papan.size())
+#         if (score > bestScore):
+#             bestScore = score
+#             bestX = pion.x
+#             bestY = pion.y
 
+#     #Pindahin pake x dan y
 
-def max(papan, depth):
-
-
-def bestMove(papan):
-    bestScore = -9999
-    move
-    for i in (self.isi):
-        
-
-
-
+#     #ganti turn
+    
+                    
 
 # MAIN
 
@@ -266,7 +392,7 @@ while run:
                     lastX, lastY = -1, -1
                     countLoop = 1
                     sedangLoncat = False
-                if pygame.Rect(board.okayhitbox).collidepoint(event.pos):
+                if pygame.Rect(board.okayhitbox).collidepoint(event.pos): #Okay hitbox
                     if (len(curPiece) > 1): #intinya minimal ada 2 cellboard yang lagi diklik
                         #dia ngubah turn dia mengubah lastclicked jadi bidak yang ada isinya, sisanya jadi bidak kosong, terus ngereset curPiece, lastX,lastY, dan sedang loncat 
                         board.changeturn()
